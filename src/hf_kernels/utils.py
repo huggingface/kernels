a7697a15f1cf96267cf861598d6339bdb49f9abf
Daniël de Kok <me@danieldk.eu>
2025-01-17 13:20:48 +0000

    PoC: allow users to lock the kernel revisions
    This change allows Python projects that use kernels to lock the
    kernel revisions on a project-basis. For this to work, the user
    only has to include `hf-kernels` as a build dependency. During
    the build, a lock file is written to the package's pkg-info.
    During runtime we can read it out and use the corresponding
    revision. When the kernel is not locked, the revision that is provided
    as an argument is used.

--- src/hf_kernels/utils.py
+++ src/hf_kernels/utils.py
@@ -1,4 +1,10 @@
+from types import ModuleType
+from typing import List, Optional
 import importlib
+import importlib.metadata
+from importlib.metadata import Distribution
+import inspect
+import json
 import platform
 import sys
 import os
@@ -50,6 +56,10 @@ def get_kernel(repo_id: str, revision: str = "main"):
 
 
 def load_kernel(repo_id: str, revision: str = "main"):
+    locked_revision = _get_caller_locked_kernel(repo_id)
+    if locked_revision is not None:
+        revision = locked_revision
+
     filename = hf_hub_download(
         repo_id, "build.toml", local_files_only=True, revision=revision
     )
@@ -59,3 +69,36 @@ def load_kernel(repo_id: str, revision: str = "main"):
     repo_path = os.path.dirname(filename)
     package_path = f"{repo_path}/build/{build_variant()}"
     return import_from_path(package_name, f"{package_path}/{package_name}/__init__.py")
+
+
+def _get_caller_locked_kernel(name: str) -> Optional[str]:
+    for dist in _get_caller_distributions():
+        lock_json = dist.read_text("hf_kernels.lock")
+        if lock_json is not None:
+            return json.loads(lock_json).get(name)
+    return None
+
+
+def _get_caller_distributions() -> List[Distribution]:
+    module = _get_caller_module()
+    if module is None:
+        return []
+
+    # Look up all possible distributions that this module could be from.
+    package = module.__name__.split(".")[0]
+    dist_names = importlib.metadata.packages_distributions().get(package)
+    if dist_names is None:
+        return []
+
+    return [importlib.metadata.distribution(dist_name) for dist_name in dist_names]
+
+
+def _get_caller_module() -> Optional[ModuleType]:
+    stack = inspect.stack()
+    # Get first module in the stack that is not the current module.
+    first_module = inspect.getmodule(stack[0][0])
+    for frame in stack[1:]:
+        module = inspect.getmodule(frame[0])
+        if module is not None and module != first_module:
+            return module
+    return first_module

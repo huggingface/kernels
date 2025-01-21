544354cb97c5e24654128b12975d77626a17f79c
Daniël de Kok <me@danieldk.eu>
2025-01-21 16:08:40 +0100

    Add support for locking kernels (#10)
    * PoC: allow users to lock the kernel revisions
    
    This change allows Python projects that use kernels to lock the
    kernel revisions on a project-basis. For this to work, the user
    only has to include `hf-kernels` as a build dependency. During
    the build, a lock file is written to the package's pkg-info.
    During runtime we can read it out and use the corresponding
    revision. When the kernel is not locked, the revision that is provided
    as an argument is used.
    
    * Generate lock files with `hf-lock-kernels`, copy to egg
    
    * Various improvements
    
    * Name CLI `hf-kernels`, add `download` subcommand
    
    * hf-kernels.lock
    
    * Bump version to 0.1.1
    
    * Use setuptools for testing the wheel
    
    * Factor out tomllib module selection
    
    * Pass through `local_files_only` in `get_metadata`
    
    * Do not reuse implementation in `load_kernel`
    
    * The tests install hf-kernels from PyPI, should be local
    
    * docker: package is in subdirectory

--- src/hf_kernels/utils.py
+++ src/hf_kernels/utils.py
@@ -1,16 +1,20 @@
 import importlib
+import importlib.metadata
+import inspect
+import json
+import os
 import platform
 import sys
-import os
+from importlib.metadata import Distribution
+from types import ModuleType
+from typing import List, Optional
 
 import torch
 from huggingface_hub import hf_hub_download, snapshot_download
 from packaging.version import parse
 
-if sys.version_info >= (3, 11):
-    import tomllib
-else:
-    import tomli as tomllib
+from hf_kernels.compat import tomllib
+from hf_kernels.lockfile import KernelLock
 
 
 def build_variant():
@@ -31,16 +35,26 @@ def import_from_path(module_name: str, file_path):
     return module
 
 
-def install_kernel(repo_id: str, revision: str):
-    package_name = get_metadata(repo_id)["torch"]["name"]
+def install_kernel(repo_id: str, revision: str, local_files_only: bool = False):
+    package_name = get_metadata(repo_id, revision, local_files_only=local_files_only)[
+        "torch"
+    ]["name"]
     repo_path = snapshot_download(
-        repo_id, allow_patterns=f"build/{build_variant()}/*", revision=revision
+        repo_id,
+        allow_patterns=f"build/{build_variant()}/*",
+        revision=revision,
+        local_files_only=local_files_only,
     )
     return package_name, f"{repo_path}/build/{build_variant()}"
 
 
-def get_metadata(repo_id: str):
-    with open(hf_hub_download(repo_id, "build.toml"), "rb") as f:
+def get_metadata(repo_id: str, revision: str, local_files_only: bool = False):
+    with open(
+        hf_hub_download(
+            repo_id, "build.toml", revision=revision, local_files_only=local_files_only
+        ),
+        "rb",
+    ) as f:
         return tomllib.load(f)
 
 
@@ -49,13 +63,70 @@ def get_kernel(repo_id: str, revision: str = "main"):
     return import_from_path(package_name, f"{package_path}/{package_name}/__init__.py")
 
 
-def load_kernel(repo_id: str, revision: str = "main"):
+def load_kernel(repo_id: str):
+    """Get a pre-downloaded, locked kernel."""
+    locked_sha = _get_caller_locked_kernel(repo_id)
+
+    if locked_sha is None:
+        raise ValueError(f"Kernel `{repo_id}` is not locked")
+
     filename = hf_hub_download(
-        repo_id, "build.toml", local_files_only=True, revision=revision
+        repo_id, "build.toml", local_files_only=True, revision=locked_sha
     )
     with open(filename, "rb") as f:
         metadata = tomllib.load(f)
     package_name = metadata["torch"]["name"]
+
     repo_path = os.path.dirname(filename)
     package_path = f"{repo_path}/build/{build_variant()}"
     return import_from_path(package_name, f"{package_path}/{package_name}/__init__.py")
+
+
+def get_locked_kernel(repo_id: str, local_files_only: bool = False):
+    """Get a kernel using a lock file."""
+    locked_sha = _get_caller_locked_kernel(repo_id)
+
+    if locked_sha is None:
+        raise ValueError(f"Kernel `{repo_id}` is not locked")
+
+    package_name, package_path = install_kernel(
+        repo_id, locked_sha, local_files_only=local_files_only
+    )
+
+    return import_from_path(package_name, f"{package_path}/{package_name}/__init__.py")
+
+
+def _get_caller_locked_kernel(repo_id: str) -> Optional[str]:
+    for dist in _get_caller_distributions():
+        lock_json = dist.read_text("hf-kernels.lock")
+        if lock_json is not None:
+            for kernel_lock_json in json.loads(lock_json):
+                kernel_lock = KernelLock.from_json(kernel_lock_json)
+                if kernel_lock.repo_id == repo_id:
+                    return kernel_lock.sha
+    return None
+
+
+def _get_caller_distributions() -> List[Distribution]:
+    module = _get_caller_module()
+    if module is None:
+        return []
+
+    # Look up all possible distributions that this module could be from.
+    package = module.__name__.split(".")[0]
+    dist_names = importlib.metadata.packages_distributions().get(package)
+    if dist_names is None:
+        return []
+
+    return [importlib.metadata.distribution(dist_name) for dist_name in dist_names]
+
+
+def _get_caller_module() -> Optional[ModuleType]:
+    stack = inspect.stack()
+    # Get first module in the stack that is not the current module.
+    first_module = inspect.getmodule(stack[0][0])
+    for frame in stack[1:]:
+        module = inspect.getmodule(frame[0])
+        if module is not None and module != first_module:
+            return module
+    return first_module

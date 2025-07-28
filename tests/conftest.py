65ebdda3a142f9656d90bf1aa408f9b4c2543aeb
Daniël de Kok <me@danieldk.eu>
2025-07-28 11:00:49 +0000

    Test examples in docstrings using mktestdocs
    Also adjust examples so that they are correct.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 import sys
 
 import pytest
+import torch
+
+has_cuda = torch.cuda.is_available() and torch.cuda.device_count() > 0
 
 
 def pytest_runtest_setup(item):
-    if "linux_only" in item.keywords and not sys.platform.startswith("linux"):
-        pytest.skip("skipping Linux-only test on non-Linux platform")
+    if "cuda_only" in item.keywords and not has_cuda:
+        pytest.skip("skipping CUDA-only test on host without CUDA")
     if "darwin_only" in item.keywords and not sys.platform.startswith("darwin"):
         pytest.skip("skipping macOS-only test on non-macOS platform")

28be6615672e4e41a7b84acfdc44d3668b11f486
sayakpaul <spsayakpaul@gmail.com>
2025-09-24 13:49:24 +0530

    up

--- examples/basic.py
+++ examples/basic.py
@@ -20,11 +20,11 @@ activation.gelu_fast(y, x)
 print("Kernel successfully executed")
 
 # Check results
-expected = torch.tensor([
-    [0.8408, 1.9551, 2.9961],
-    [4.0000, 5.0000, 6.0000],
-    [7.0000, 8.0000, 9.0000]
-], device='cuda:0', dtype=torch.float16)
+expected = torch.tensor(
+    [[0.8408, 1.9551, 2.9961], [4.0000, 5.0000, 6.0000], [7.0000, 8.0000, 9.0000]],
+    device="cuda:0",
+    dtype=torch.float16,
+)
 assert torch.allclose(y, expected)
 
 print("Calculated values are exact")

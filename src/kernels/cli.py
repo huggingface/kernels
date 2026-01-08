254490825c1396ac92e28134c4b9906743e65f37
Daniël de Kok <me@danieldk.eu>
2026-01-08 14:22:01 +0000

    Tighten up build variant check
    To avoid that we use the source directory containing `torch-ext`
    to be uploaded.

--- src/kernels/cli.py
+++ src/kernels/cli.py
@@ -13,7 +13,7 @@ from kernels.utils import install_kernel, install_kernel_all_variants
 
 from .doc import generate_readme_for_kernel
 
-BUILD_VARIANT_REGEX = re.compile(r"^(torch\d+\d+|torch-)")
+BUILD_VARIANT_REGEX = re.compile(r"^(torch\d+\d+|torch-(cpu|cuda|metal|rocm|xpu))")
 
 
 def main():

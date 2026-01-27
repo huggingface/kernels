9d22effcd0ce8cdde3cd5d34730d2801afac7fa8
Daniël de Kok <me@danieldk.eu>
2026-01-27 13:36:11 +0100

    fix bug when kernels upload (#242)
    Signed-off-by: Liu, Kaixuan <kaixuan.liu@intel.com>
    Co-authored-by: Liu, Kaixuan <kaixuan.liu@intel.com>

--- build2cmake/src/templates/xpu/torch-extension.cmake
+++ build2cmake/src/templates/xpu/torch-extension.cmake
@@ -10,3 +10,14 @@ define_gpu_extension_target(
 # Add XPU/SYCL specific linker flags
 target_link_options({{ ops_name }} PRIVATE ${sycl_link_flags})
 target_link_libraries({{ ops_name }} PRIVATE dnnl)
+
+{% if platform == 'windows' %}
+# These methods below should be included from preamble.cmake on windows platform.
+
+# Add kernels_install target for huggingface/kernels library layout
+add_kernels_install_target({{ ops_name }} "{{ name }}" "${BUILD_VARIANT_NAME}")
+
+# Add local_install target for local development with get_local_kernel()
+add_local_install_target({{ ops_name }} "{{ name }}" "${BUILD_VARIANT_NAME}")
+
+{% endif %}

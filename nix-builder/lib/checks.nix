d716f45fc0afebaaf6892a533d854c67de9cb08a
Daniël de Kok <me@danieldk.eu>
2026-03-25 13:55:31 +0000

    nix fmt

--- nix-builder/lib/checks.nix
+++ nix-builder/lib/checks.nix
@@ -12,9 +12,10 @@ let
     path = ../../examples/kernels/relu-torch-bounds;
   };
 in
-assert lib.assertMsg (builtins.all (buildSet: buildSet.torch.version == "2.10.0") kernelBuildSets) ''
-  Torch minver/maxver filtering does not work.
-'';
+assert lib.assertMsg (builtins.all (buildSet: buildSet.torch.version == "2.10.0") kernelBuildSets)
+  ''
+    Torch minver/maxver filtering does not work.
+  '';
 runCommand "builder-nix-checks" { } ''
   touch $out
 ''

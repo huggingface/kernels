8bc6d6a1475e2acad8eb7a2f99caed6cede185aa
GitGlimpse895 <sayakmondal432@gmail.com>
2026-04-18 12:06:52 +0530

    layer/globals: add _CACHED_LAYER ContextVar alongside _KERNEL_MAPPING

--- kernels/src/kernels/layer/globals.py
+++ kernels/src/kernels/layer/globals.py
@@ -1,8 +1,15 @@
 import os
 from contextvars import ContextVar
+from typing import TYPE_CHECKING, Type
 
 from .repos import DeviceRepos
+
+if TYPE_CHECKING:
+    from torch import nn
+    from .repos import RepositoryProtocol
 
 _DISABLE_KERNEL_MAPPING: bool = bool(int(os.environ.get("DISABLE_KERNEL_MAPPING", "0")))
 
 _KERNEL_MAPPING: ContextVar[dict[str, dict[str, DeviceRepos]]] = ContextVar("_KERNEL_MAPPING", default={})
+
+_CACHED_LAYER: ContextVar[dict["RepositoryProtocol", Type["nn.Module"]]] = ContextVar("_CACHED_LAYER", default={})

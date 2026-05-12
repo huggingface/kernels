459842db9ca3b3f30e43e1e9a8042c40f22a4ed6
Daniël de Kok <me@danieldk.eu>
2026-05-12 15:55:38 +0000

    Working toolchain that can compile simple hello world

--- nix-builder/pkgs/manylinux/default.nix
+++ nix-builder/pkgs/manylinux/default.nix
@@ -20,6 +20,39 @@ let
     (import ./components.nix)
     # Overrides (adding dependencies, etc.)
     (import ./overrides.nix)
+
+    (final: prev: {
+      toolchain = pkgs.stdenvNoCC.mkDerivation {
+        pname = "toolchain";
+        version = final.gcc-toolset-14-gcc.version;
+
+        nativeBuildInputs = with pkgs; [ rsync ];
+
+        dontUnpack = true;
+
+        installPhase = with final; ''
+          runHook preInstall
+
+          mkdir $out
+          for path in ${gcc-toolset-14-binutils} ${gcc-toolset-14-gcc} ${gcc-toolset-14-gcc-cxx} ${gcc-toolset-14-libstdcxx-devel} ${glibc} ${glibc-headers} ${kernel-headers}; do
+            rsync --exclude=nix-support -a $path/ $out/
+          done
+
+          runHook postInstall
+        '';
+      };
+
+      stdenv = pkgs.overrideCC pkgs.stdenv (
+        pkgs.wrapCCWith {
+          cc = final.toolchain;
+          libc = final.glibc;
+          bintools = pkgs.wrapBintoolsWith {
+            bintools = final.gcc-toolset-14-binutils;
+            libc = final.glibc;
+          };
+        }
+      );
+    })
   ];
 in
 lib.makeScope newScope (lib.extends composed fixedPoint)

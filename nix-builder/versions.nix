e7773dab269bd255fa07ce33b3b9661b24d0f9f3
Daniël de Kok <me@danieldk.eu>
2026-07-28 09:24:35 +0000

    python3Packages.torch: 2.13.0-rc15 -> 2.13.0

--- nix-builder/versions.nix
+++ nix-builder/versions.nix
@@ -1,6 +1,6 @@
 [
   {
-    torchVersion = "2.11";
+    torchVersion = "2.12";
     cpu = true;
     systems = [
       "aarch64-darwin"
@@ -11,7 +11,7 @@
     tvmFfiVersion = "0.1";
   }
   {
-    torchVersion = "2.11";
+    torchVersion = "2.12";
     cudaVersion = "12.6";
     systems = [
       "x86_64-linux"
@@ -21,71 +21,6 @@
     tvmFfiVersion = "0.1";
   }
   {
-    torchVersion = "2.11";
-    cudaVersion = "12.8";
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    bundleBuild = true;
-    tvmFfiVersion = "0.1";
-  }
-  {
-    torchVersion = "2.11";
-    cudaVersion = "13.0";
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    bundleBuild = true;
-    tvmFfiVersion = "0.1";
-  }
-  {
-    torchVersion = "2.11";
-    metal = true;
-    systems = [ "aarch64-darwin" ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.11";
-    rocmVersion = "7.1";
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.11";
-    rocmVersion = "7.2";
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.11";
-    xpuVersion = "2025.3.2";
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-    tvmFfiVersion = "0.1";
-  }
-
-  {
-    torchVersion = "2.12";
-    cpu = true;
-    systems = [
-      "aarch64-darwin"
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.12";
-    cudaVersion = "12.6";
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    bundleBuild = true;
-  }
-  {
     torchVersion = "2.12";
     cudaVersion = "13.0";
     systems = [
@@ -93,6 +28,7 @@
       "aarch64-linux"
     ];
     bundleBuild = true;
+    tvmFfiVersion = "0.1";
   }
   {
     torchVersion = "2.12";
@@ -127,6 +63,7 @@
     xpuVersion = "2025.3.2";
     systems = [ "x86_64-linux" ];
     bundleBuild = true;
+    tvmFfiVersion = "0.1";
   }
 
   {

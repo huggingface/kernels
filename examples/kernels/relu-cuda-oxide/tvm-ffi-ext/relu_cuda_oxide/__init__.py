5e727101bca47a7ecbdfebecb1dd44960cf971e2
David Holtz <david.richard.holtz@gmail.com>
2026-08-03 17:55:32 +0000

    feat: add cuda relu example
    Signed-off-by: David Holtz <david.richard.holtz@gmail.com>

---
+++ examples/kernels/relu-cuda-oxide/tvm-ffi-ext/relu_cuda_oxide/__init__.py
@@ -0,0 +1,15 @@
+import tvm_ffi
+
+from ._ops import ops
+
+
+def relu(x, out):
+    x_t = tvm_ffi.from_dlpack(x)
+    out_t = tvm_ffi.from_dlpack(out)
+
+    device = x_t.device
+    if device.type != "cuda":
+        raise NotImplementedError(f"Unsupported device type: {device.type}")
+    ops.relu(x_t, out_t)
+
+    return out
